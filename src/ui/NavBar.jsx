3400e1db87643f20ea5f02d394e540b46bf44351
daftnemesis <XxdaftnemesisxX@gmail.com>
2022-06-29 23:12:19 -0500

    dandole estilos al navbar

--- src/ui/NavBar.jsx
+++ src/ui/NavBar.jsx
@@ -1,31 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 export const NavBar = () => {
+
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  }
+
+  console.log(toggle);
+
   return (
-    <nav className='bg-orange-500 flex justify-between'>
-      <div>
-        <NavLink to='/'>
-          <div className='flex align-middle'>
-            <img 
-              src="../assets/logo.png" 
-              alt="" 
-              className="h-12 w-12"
-            />
-            <p className='self-center font-heading text-3xl text-white'>Decoraciones Chapis</p>
-          </div>
+    <nav className='bg-amber-100 border-amber-600 px-2 sm:px-4 py-2.5 rounded shadow-md'>
+      <div className='container flex flex-wrap justify-between items-center mx-auto'>
+
+        <NavLink 
+          to='/'
+          className='flex items-center'
+        >
+          <img 
+            src="../assets/logo.png" 
+            alt="chapis logo" 
+            className="mr-1 h-9 sm:h-12"
+          />
+          <span className='self-center font-heading text-3xl text-sky-900 whitespace-nowrap'>Decoraciones Chapis</span>
         </NavLink>
+        <button 
+          className='ml-3 md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200'
+          onClick={handleToggle}  
+        >
+          <span className='sr-only'>Abrir menu principal</span>
+          <svg className="w-6 h-6 fill-sky-900 bg-amber-100" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+          <svg className="hidden w-6 h-6 fill-sky-900" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        </button>
+
+        <div className={`flex w-full md:block md:w-auto ${toggle ? '': 'hidden'}`} id='mobile-menu'>
+          <ul className='flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium'>
+            <li className='text-sky-900'>Inicio</li>
+            <li className='text-sky-900'>Acerca De</li>
+            <li className='text-sky-900'>Contacto</li>
+          </ul>
+        </div>
+
       </div>
-
-      <div className='flex'>
-        <ul className='flex self-center'>
-          <li className=''>Home</li>
-          <li>Acerca De</li>
-          <li>Contacto</li>
-        </ul>
-      </div>
-
-
     </nav>
   )
 }
